a9dd4a1a17086c19713430881ed25c5b3b09f886
Jussi Kalliomäki <jussi.kalliomaki99@gmail.com>
2020-12-09 19:58:26 +0200

    first revisions of all opcodes

--- chip8.cpp
+++ chip8.cpp
@@ -65,6 +65,11 @@ void chip8::emulateCycle()
 	// Fetch opcode by merging two bytes together
 	opcode = memory[pc] << 8 | memory[pc + 1];
 
+
+	int tempInt;
+	unsigned short tempShort1;
+	unsigned short tempShort2;
+	unsigned short tempShort3;
 	// Decode opcode
 	// Switch for MSB
 	switch (opcode & 0xF000)
@@ -253,19 +258,130 @@ void chip8::emulateCycle()
 
 		// dxyn
 	case 0xD000:
+		tempShort1 = (opcode & 0x000F);
+		tempShort2 = R[(opcode & 0x0F00) >> 8];
+		tempShort3 = R[(opcode & 0x00F0) >> 4];
+		R[0xF] = 0;
 
+		for (int row = 0; row < tempShort1; row++) {
+			char rowbyte = memory[I + row];
+			for (int line = 0; line < 8; line++) {
+				// If bit to be drawn is not zero
+				if ((rowbyte & (0x80 >> line)) != 0) {
+					if (gfx[tempShort2 + line + ((tempShort3 + row) * 64)] == 1){
+						R[0xF] = 1;
+
+					gfx[tempShort2 + line + ((tempShort3 + row) * 64)] ^= 1;
+					}
+				}
+			}
+		}
+		pc += 2;
+		break;
 
 		// ex9e
+	case 0xE000:
+		switch (opcode & 0x000F) {
+		case 0x000E:
+			if (key[R[(opcode & 0x0F00) >> 8]] == 1) {
+				pc += 4;
+			}
+			else {
+				pc += 2;
+			}
+			break;
+
 		// exa1
+		case 0x0001:
+			if (key[R[(opcode & 0x0F00) >> 8]] == 0) {
+				pc += 4;
+			}
+			else {
+				pc += 2;
+			}
+			break;
+
+		default:
+			printf("opcode not found [0xE000] %X\n", opcode);
+			break;
+		}
+	case 0xF000:
+		switch (opcode & 0x000F)
+		{
 		// fx07
-		// fx0a
-		// fx15
+		case 0x0007:
+			R[(opcode & 0x0F00) >> 8] = delay_timer;
+			pc += 2;
+			break;
+
 		// fx18
+		case 0x0008:
+			R[(opcode & 0x0F00) >> 8] = sound_timer;
+			pc += 2;
+			break;
+
+		// fx0a
+		case 0x000A:
+			printf("not made yet %X\n", opcode);
+			pc += 2;
+			break;
+
 		// fx1e
+		case 0x000E:
+			I += R[(opcode & 0x0F00) >> 8];
+			pc += 2;
+			break;
+
 		// fx29
+		case 0x0009:
+			printf("not made yet %X\n", opcode);
+			pc += 2;
+			break;
+
 		// fx33
-		// fx55
-		// fx65
+		case 0x0003:
+			tempInt = R[(opcode & 0x0F00) >> 8];
+			memory[I] = tempInt / 100;
+			memory[I + 1] = (tempInt - (int(memory[I]) * 100)) / 10;
+			memory[I + 2] = tempInt % 10;
+			pc += 2;
+			break;
+
+		case 0x0005:
+			switch (opcode & 0x00F0) {
+			// fx15
+			case 0x0010:
+				delay_timer = R[(opcode & 0x0F00) >> 8];
+				pc += 2;
+				break;
+
+			// fx55
+			case 0x0050:
+				tempInt = (opcode & 0x0F00) >> 8;
+
+				for (int j = 0; j <= tempInt; j++) {
+					memory[I + j] = R[j];
+				}
+				pc += 2;
+				break;
+
+			// fx65
+			case 0x0060:
+				tempInt = (opcode & 0x0F00) >> 8;
+
+				for (int j = 0; j <= tempInt; j++) {
+					R[j] = memory[I + j];
+				}
+				pc += 2;
+				break;
+
+			default:
+				printf("opcode not found [0xF005] %X\n", opcode);
+			}
+
+
+			break;
+		}
 
 
 	default:
