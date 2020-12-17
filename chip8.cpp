0af512a3734856e226b8da6d87cdcab38803ef87
Jussi Kalliomäki <jussi.kalliomaki99@gmail.com>
2020-12-17 17:55:32 +0200

    fix more opcodes

--- chip8.cpp
+++ chip8.cpp
@@ -107,346 +107,313 @@ bool chip8::load(const char gameFileName[])
 
 void chip8::emulateCycle()
 {
-	// Fetch opcode by merging two bytes together
+	// Fetch opcode
 	opcode = memory[pc] << 8 | memory[pc + 1];
 
-
-	int tempInt;
-	unsigned short tempShort1;
-	unsigned short tempShort2;
-	unsigned short tempShort3;
-	// Decode opcode
-	// Switch for MSB
+	// Process opcode
 	switch (opcode & 0xF000)
 	{
 	case 0x0000:
 		switch (opcode & 0x000F)
 		{
-		// 0x00E0, clear display
-		case 0x0000:
-			disp_clear();
+		case 0x0000: // 0x00E0: Clears the screen
+			for (int i = 0; i < 2048; ++i)
+				gfx[i] = 0x0;
 			drawFlag = true;
 			pc += 2;
 			break;
 
-			// 0x00EE, return from subroutine
-		case 0x000e:
-			--sp;
-			pc = stack[sp];
-			pc += 2;
+		case 0x000E: // 0x00EE: Returns from subroutine
+			--sp;			// 16 levels of stack, decrease stack pointer to prevent overwrite
+			pc = stack[sp];	// Put the stored return address from the stack back into the program counter					
+			pc += 2;		// Don't forget to increase the program counter!
 			break;
 
 		default:
 			printf("Unknown opcode [0x0000]: 0x%X\n", opcode);
-			pc += 2;
 		}
+		break;
 
-		// 1nnn
-	case 0x1000:
+	case 0x1000: // 0x1NNN: Jumps to address NNN
 		pc = opcode & 0x0FFF;
 		break;
 
-		// 2nnn 
-	case 0x2000:
-		stack[sp] = pc;
-		++sp;
-		pc = opcode & 0x0FFF;
+	case 0x2000: // 0x2NNN: Calls subroutine at NNN.
+		stack[sp] = pc;			// Store current address in stack
+		++sp;					// Increment stack pointer
+		pc = opcode & 0x0FFF;	// Set the program counter to the address at NNN
 		break;
 
-		// 3xnn
-	case 0x3000:
+	case 0x3000: // 0x3XNN: Skips the next instruction if RX equals NN
 		if (R[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
-		{
 			pc += 4;
-		}
-		else {
+		else
 			pc += 2;
-		}
 		break;
 
-		// 4xnn
-	case 0x4000:
+	case 0x4000: // 0x4XNN: Skips the next instruction if RX doesn't equal NN
 		if (R[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
-		{
 			pc += 4;
-		}
-		else {
+		else
 			pc += 2;
-		}
 		break;
 
-		// 5xy0
-	case 0x5000:
+	case 0x5000: // 0x5XY0: Skips the next instruction if RX equals RY.
 		if (R[(opcode & 0x0F00) >> 8] == R[(opcode & 0x00F0) >> 4])
-		{
 			pc += 4;
-		}
-		else {
+		else
 			pc += 2;
-		}
+		break;
 
-		// 6xnn
-	case 0x6000:
+	case 0x6000: // 0x6XNN: Sets RX to NN.
 		R[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
 		pc += 2;
 		break;
 
-		// 7xnn
-	case 0x7000:
+	case 0x7000: // 0x7XNN: Adds NN to RX.
 		R[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
 		pc += 2;
 		break;
 
 	case 0x8000:
-		switch (opcode & 0x000F) {
-		// 8xy0
-		case 0x0000:
+		switch (opcode & 0x000F)
+		{
+		case 0x0000: // 0x8XY0: Sets RX to the value of RY
 			R[(opcode & 0x0F00) >> 8] = R[(opcode & 0x00F0) >> 4];
 			pc += 2;
 			break;
 
-		// 8xy1
-		case 0x0001:
-			R[(opcode & 0x0F00) >> 8] = R[(opcode & 0x0F00) >> 8] | R[(opcode & 0x00F0) >> 4];
+		case 0x0001: // 0x8XY1: Sets RX to "RX OR RY"
+			R[(opcode & 0x0F00) >> 8] |= R[(opcode & 0x00F0) >> 4];
 			pc += 2;
 			break;
 
-		// 8xy2
-		case 0x0002:
-			R[(opcode & 0x0F00) >> 8] = R[(opcode & 0x0F00) >> 8] & R[(opcode & 0x00F0) >> 4];
+		case 0x0002: // 0x8XY2: Sets RX to "RX AND RY"
+			R[(opcode & 0x0F00) >> 8] &= R[(opcode & 0x00F0) >> 4];
 			pc += 2;
 			break;
 
-		// 8xy3
-		case 0x0003:
-			R[(opcode & 0x0F00) >> 8] = R[(opcode & 0x0F00) >> 8] ^ R[(opcode & 0x00F0) >> 4];
+		case 0x0003: // 0x8XY3: Sets RX to "RX XOR RY"
+			R[(opcode & 0x0F00) >> 8] ^= R[(opcode & 0x00F0) >> 4];
 			pc += 2;
 			break;
 
-		// 8xy4
-		case 0x0004:
+		case 0x0004: // 0x8XY4: Adds RY to RX. RF is set to 1 when there's a carry, and to 0 when there isn't					
+			if (R[(opcode & 0x00F0) >> 4] > (0xFF - R[(opcode & 0x0F00) >> 8]))
+				R[0xF] = 1; //carry
+			else
+				R[0xF] = 0;
 			R[(opcode & 0x0F00) >> 8] += R[(opcode & 0x00F0) >> 4];
-			if (R[(opcode & 0x00F0) >> 4] > R[(opcode & 0x0F00) >> 8]) {
-				R[0xF] = 1;
-			}
-			else {
-				R[0xF] = 0;
-			}
 			pc += 2;
 			break;
 
-		// 8xy5
-		case 0x0005:
+		case 0x0005: // 0x8XY5: RY is subtracted from RX. RF is set to 0 when there's a borrow, and 1 when there isn't
+			if (R[(opcode & 0x00F0) >> 4] > R[(opcode & 0x0F00) >> 8])
+				R[0xF] = 0; // there is a borrow
+			else
+				R[0xF] = 1;
 			R[(opcode & 0x0F00) >> 8] -= R[(opcode & 0x00F0) >> 4];
-			if (R[(opcode & 0x0F00) >> 8] < R[(opcode & 0x00F0) >> 4])
-			{
-				R[0xF] = 1;
-			}
-			else {
-				R[0xF] = 0;
-			}
 			pc += 2;
 			break;
 
-		// 8xy6
-		case 0x0006:
-			R[0xF] = R[(opcode & 0x0F00) >> 8] & 0x0001;
+		case 0x0006: // 0x8XY6: Shifts RX right by one. RF is set to the value of the least significant bit of RX before the shift
+			R[0xF] = R[(opcode & 0x0F00) >> 8] & 0x1;
 			R[(opcode & 0x0F00) >> 8] >>= 1;
 			pc += 2;
 			break;
 
-		// 8xy7
-		case 0x0007:
+		case 0x0007: // 0x8XY7: Sets RX to RY minus RX. RF is set to 0 when there's a borrow, and 1 when there isn't
+			if (R[(opcode & 0x0F00) >> 8] > R[(opcode & 0x00F0) >> 4])	// RY-RX
+				R[0xF] = 0; // there is a borrow
+			else
+				R[0xF] = 1;
 			R[(opcode & 0x0F00) >> 8] = R[(opcode & 0x00F0) >> 4] - R[(opcode & 0x0F00) >> 8];
-			if (R[(opcode & 0x0F00) >> 8] > R[(opcode & 0x00F0) >> 4])
-			{
-				R[0xF] = 0;
-			}
-			else {
-				R[0xF] = 1;
-			}
 			pc += 2;
 			break;
 
-		// 8xye
-		case 0x000E:
-			R[0xF] = R[(opcode & 0x8000) >> 16];
-			R[(opcode & 0x0F00) >> 8] >>= 1;
+		case 0x000E: // 0x8XYE: Shifts RX left by one. RF is set to the value of the most significant bit of RX before the shift
+			R[0xF] = R[(opcode & 0x0F00) >> 8] >> 7;
+			R[(opcode & 0x0F00) >> 8] <<= 1;
 			pc += 2;
 			break;
-
 
 		default:
-			printf("opcode not found [0x8000] %X\n", opcode);
-			pc += 2;
+			printf("Unknown opcode [0x8000]: 0x%X\n", opcode);
 		}
 		break;
 
-		// 9xy0
-	case 0x9000:
+	case 0x9000: // 0x9XY0: Skips the next instruction if RX doesn't equal RY
 		if (R[(opcode & 0x0F00) >> 8] != R[(opcode & 0x00F0) >> 4])
-		{
 			pc += 4;
-		}
-		else {
+		else
 			pc += 2;
-		}
 		break;
 
-		// annn
-	case 0xA000:
-		I = memory[opcode & 0x0FFF];
+	case 0xA000: // ANNN: Sets I to the address NNN
+		I = opcode & 0x0FFF;
 		pc += 2;
 		break;
 
-		// bnnn
-	case 0xB000:
-		pc = R[0] + (opcode & 0x0FFF);
+	case 0xB000: // BNNN: Jumps to the address NNN plus R0
+		pc = (opcode & 0x0FFF) + R[0];
 		break;
 
-		// cxnn
-	case 0xC000:
-		R[(opcode & 0x0F00) >> 8] = rand() % 256;
+	case 0xC000: // CXNN: Sets RX to a random number and NN
+		R[(opcode & 0x0F00) >> 8] = (rand() % 0xFF) & (opcode & 0x00FF);
 		pc += 2;
 		break;
 
-		// dxyn
-	case 0xD000:
-		tempShort1 = (opcode & 0x000F);
-		tempShort2 = R[(opcode & 0x0F00) >> 8];
-		tempShort3 = R[(opcode & 0x00F0) >> 4];
+	case 0xD000: // DXYN: Draws a sprite at coordinate (RX, RY) that has a width of 8 pixels and a height of N pixels. 
+				 // Each row of 8 pixels is read as bit-coded starting from memory location I; 
+				 // I value doesn't change after the execution of this instruction. 
+				 // RF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, 
+				 // and to 0 if that doesn't happen
+	{
+		unsigned short x = R[(opcode & 0x0F00) >> 8];
+		unsigned short y = R[(opcode & 0x00F0) >> 4];
+		unsigned short height = opcode & 0x000F;
+		unsigned short pixel;
+
 		R[0xF] = 0;
-
-		for (int row = 0; row < tempShort1; row++) {
-			char rowbyte = memory[I + row];
-			for (int line = 0; line < 8; line++) {
-				// If bit to be drawn is not zero
-				if ((rowbyte & (0x80 >> line)) != 0) {
-					if (gfx[tempShort2 + line + ((tempShort3 + row) * 64)] == 1){
+		for (int yline = 0; yline < height; yline++)
+		{
+			pixel = memory[I + yline];
+			for (int xline = 0; xline < 8; xline++)
+			{
+				if ((pixel & (0x80 >> xline)) != 0)
+				{
+					if (gfx[(x + xline + ((y + yline) * 64))] == 1)
+					{
 						R[0xF] = 1;
-
-					gfx[tempShort2 + line + ((tempShort3 + row) * 64)] ^= 1;
 					}
+					gfx[x + xline + ((y + yline) * 64)] ^= 1;
 				}
 			}
 		}
-		pc += 2;
-		drawFlag = true;
-		break;
 
-		// ex9e
+		drawFlag = true;
+		pc += 2;
+	}
+	break;
+
 	case 0xE000:
-		switch (opcode & 0x000F) {
-		case 0x000E:
-			if (key[R[(opcode & 0x0F00) >> 8]] == 1) {
+		switch (opcode & 0x00FF)
+		{
+		case 0x009E: // EX9E: Skips the next instruction if the key stored in RX is pressed
+			if (key[R[(opcode & 0x0F00) >> 8]] != 0)
 				pc += 4;
-			}
-			else {
+			else
 				pc += 2;
-			}
 			break;
 
-		// exa1
-		case 0x0001:
-			if (key[R[(opcode & 0x0F00) >> 8]] == 0) {
+		case 0x00A1: // EXA1: Skips the next instruction if the key stored in RX isn't pressed
+			if (key[R[(opcode & 0x0F00) >> 8]] == 0)
 				pc += 4;
-			}
-			else {
+			else
 				pc += 2;
-			}
 			break;
 
 		default:
-			printf("opcode not found [0xE000] %X\n", opcode);
-			pc += 2;
+			printf("Unknown opcode [0xE000]: 0x%X\n", opcode);
 		}
+		break;
+
 	case 0xF000:
 		switch (opcode & 0x00FF)
 		{
-		// fx07
-		case 0x0007:
+		case 0x0007: // FX07: Sets RX to the value of the delay timer
 			R[(opcode & 0x0F00) >> 8] = delay_timer;
 			pc += 2;
 			break;
 
-			// fx0a
-		case 0x000A:
-			printf("not made yet %X\n", opcode);
-			pc += 2;
-			break;
+		case 0x000A: // FX0A: A key press is awaited, and then stored in RX		
+		{
+			bool keyPress = false;
 
-			// fx15
-		case 0x0015:
-			delay_timer = R[(opcode & 0x0F00) >> 8];
-			pc += 2;
-			break;
-
-		// fx18
-		case 0x0018:
-			R[(opcode & 0x0F00) >> 8] = sound_timer;
-			pc += 2;
-			break;
-
-		// fx1e
-		case 0x001E:
-			I += R[(opcode & 0x0F00) >> 8];
-			pc += 2;
-			break;
-
-		// fx29
-		case 0x0029:
-			printf("not made yet %X\n", opcode);
-			pc += 2;
-			break;
-
-		// fx33
-		case 0x0033:
-			tempInt = R[(opcode & 0x0F00) >> 8];
-			memory[I] = tempInt / 100;
-			memory[I + 1] = (tempInt - (int(memory[I]) * 100)) / 10;
-			memory[I + 2] = tempInt % 10;
-			pc += 2;
-			break;
-
-			// fx55
-		case 0x0055:
-			tempInt = (opcode & 0x0F00) >> 8;
-
-			for (int j = 0; j <= tempInt; j++) {
-				memory[I + j] = R[j];
+			for (int i = 0; i < 16; ++i)
+			{
+				if (key[i] != 0)
+				{
+					R[(opcode & 0x0F00) >> 8] = i;
+					keyPress = true;
+				}
 			}
-			pc += 2;
-			break;
 
-		// fx65
-		case 0x0065:
-			tempInt = (opcode & 0x0F00) >> 8;
+			// If we didn't received a keypress, skip this cycle and try again.
+			if (!keyPress)
+				return;
 
-			for (int j = 0; j <= tempInt; j++) {
-				R[j] = memory[I + j];
-			}
-			pc += 2;
-			break;
-
-		default:
-			printf("opcode not found [0xF000] %X\n", opcode);
 			pc += 2;
 		}
 		break;
 
+		case 0x0015: // FX15: Sets the delay timer to RX
+			delay_timer = R[(opcode & 0x0F00) >> 8];
+			pc += 2;
+			break;
+
+		case 0x0018: // FX18: Sets the sound timer to RX
+			sound_timer = R[(opcode & 0x0F00) >> 8];
+			pc += 2;
+			break;
+
+		case 0x001E: // FX1E: Adds RX to I
+			if (I + R[(opcode & 0x0F00) >> 8] > 0xFFF)	// RF is set to 1 when range overflow (I+RX>0xFFF), and 0 when there isn't.
+				R[0xF] = 1;
+			else
+				R[0xF] = 0;
+			I += R[(opcode & 0x0F00) >> 8];
+			pc += 2;
+			break;
+
+		case 0x0029: // FX29: Sets I to the location of the sprite for the character in RX. Characters 0-F (in hexadecimal) are represented by a 4x5 font
+			I = R[(opcode & 0x0F00) >> 8] * 0x5;
+			pc += 2;
+			break;
+
+		case 0x0033: // FX33: Stores the Binary-coded decimal representation of RX at the addresses I, I plus 1, and I plus 2
+			memory[I] = R[(opcode & 0x0F00) >> 8] / 100;
+			memory[I + 1] = (R[(opcode & 0x0F00) >> 8] / 10) % 10;
+			memory[I + 2] = (R[(opcode & 0x0F00) >> 8] % 100) % 10;
+			pc += 2;
+			break;
+
+		case 0x0055: // FX55: Stores R0 to RX in memory starting at address I					
+			for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
+				memory[I + i] = R[i];
+
+			// On the original interpreter, when the operation is done, I = I + X + 1.
+			I += ((opcode & 0x0F00) >> 8) + 1;
+			pc += 2;
+			break;
+
+		case 0x0065: // FX65: Fills R0 to RX with values from memory starting at address I					
+			for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
+				R[i] = memory[I + i];
+
+			// On the original interpreter, when the operation is done, I = I + X + 1.
+			I += ((opcode & 0x0F00) >> 8) + 1;
+			pc += 2;
+			break;
+
+		default:
+			printf("Unknown opcode [0xF000]: 0x%X\n", opcode);
+		}
+		break;
 
 	default:
-		printf("Unknown opcode 0x%X\n", opcode);
-		pc += 2;
+		printf("Unknown opcode: 0x%X\n", opcode);
 	}
-
 
 	// Update timers
-	if (delay_timer > 0) {
-		delay_timer--;
-	}
-	if (sound_timer > 0) {
-		sound_timer--;
+	if (delay_timer > 0)
+		--delay_timer;
+
+	if (sound_timer > 0)
+	{
+		if (sound_timer == 1)
+			printf("BEEP!\n");
+		--sound_timer;
 	}
 
 }
